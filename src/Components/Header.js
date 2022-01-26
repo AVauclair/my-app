eb4c2b8ea37cab655cabaa2d65f52cf91f99ec5b
Vauclair <lyzha100kilo@mail.ru>
2022-01-26 19:03:11 +0700

    начало работы

---
+++ src/Components/Header.js
@@ -0,0 +1,12 @@
+function Header() {
+    return(
+      <div id = "header">
+        <a href = "#">Домой</a>
+        <a href = "#">На завод</a>
+        <a href = "#">На учебу</a>
+        <a href = "#">На заре</a>
+      </div>
+    );
+  }
+
+  export default Header;
---
+++ src/Components/Information.js
@@ -0,0 +1,13 @@
+function Information() {
+    return(
+      <div>
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+          <br/>
+          Besides, I can do things like THIS. IMPRESSIVE, HUH?
+        </p>
+      </div>
+    );
+  }
+
+  export default Information;
