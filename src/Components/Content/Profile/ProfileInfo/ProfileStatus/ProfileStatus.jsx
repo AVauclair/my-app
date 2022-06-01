ee14cafd32e55b73a129bcc1cf29a710025a1b00
Vauclair <lyzha100kilo@mail.ru>
2022-06-01 11:54:31 +0700

    переписали локальный стейт в компоненте ПрофайлСтатус с использованием хука ЮзСтейт (осталась проблема с тем, что статус приходит раньше, чем компонента рендерится, поэтому при редактировании он в инпут заносит пустую строку)

--- src/Components/Content/Profile/ProfileInfo/ProfileStatus/ProfileStatus.jsx
+++ src/Components/Content/Profile/ProfileInfo/ProfileStatus/ProfileStatus.jsx
@@ -1,43 +1,29 @@
-import React from "react"
+import React, { useState } from "react"
 
-class ProfileStatus extends React.Component {
-    state = {
-        editMode: false,
-        userStatus: this.props.userStatus,
-    }
+const ProfileStatus = (props) => {
+    let [editMode, setEditMode] = useState(false)
+    let [userStatus, setUserStatus] = useState(props.userStatus)
 
-    activateEditMode = () => {
-        this.setState({editMode: true})
-    }
-    deactivateEditMode = () => {
-        this.setState({editMode: false})
-        this.props.updateUserStatus(this.state.userStatus)
-    }
-    onChangeUserStatus = (e) => {
-        this.setState({
-        userStatus: e.currentTarget.value
-        })
+    const activateEditMode = () => {
+        setEditMode(true)
     }
 
-    componentDidUpdate = (prevProps, prevState) => {
-        if (prevProps.userStatus !== this.props.userStatus) {
-            this.setState({
-                userStatus: this.props.userStatus
-            })
-        }
+    debugger
+    const deactivateEditMode = () => {
+        setEditMode(false)
+        props.updateUserStatus(userStatus)
     }
-    //СТАТУСЫ У ТЕБЯ РАБОТАЮТ, ПРОСТО КОМПОНЕНТА РЕНДЕРИТСЯ БЫСТРЕЕ, ЧЕМ ПРИХОДИТ ЮЗЕРСТАТУС, ПОЭТОМУ ОНО ОТОБРАЖАЕТ, ЯКОБЫ ЕГО НЕТ.//
-    //КСТАТИ У ТЕБЯ ЕЩЁ ИЗ-ЗА РЕДИРЕКТА В ПРОФИЛЕ ПРИ ПОПЫТКЕ ПЕРЕЙТИ НА ЧЕЙ-ТО ПРОФИЛЬ ЧЕРЕЗ УРЛ, ОТПРАВИТ ПОЛЬЗОВАТЕЛЯ В СВОЙ ПРОФИЛЬ => 
-    //(РЕДИРЕКТИТ НА ЛОГИН ЕСЛИ ISAUTH = FALSE, А ОН ВСЕГДА БУДЕТ FALSE, ПОТОМУ ЧТО ПРИХОДИТ ПОЗЖЕ, А ПОСЛЕ ТОГО, КАК ПРИДЕТ, ЛОГИН РЕДИРЕКТНЕТ НА ТВОЙ ПРОФИЛЬ) 
-    render() {
-        return (
-            <div>
-                {this.state.editMode
-                ? <div><input onChange={this.onChangeUserStatus} autoFocus={true} onBlur={this.deactivateEditMode} value={this.state.userStatus}></input></div>
-                : <div><span onClick={this.activateEditMode}>{this.state.userStatus || "----"}</span></div>}
-            </div>
-        )
+    const onChangeUserStatus = (e) => {
+        setUserStatus(e.currentTarget.value)
     }
+
+    return (
+        <div>
+            {editMode
+                ? <div><input onChange={onChangeUserStatus} autoFocus={true} onBlur={deactivateEditMode} value={userStatus}></input></div>
+                : <div><span onClick={activateEditMode}>{props.userStatus || "----"}</span></div>}
+        </div>
+    )
 }
 
 export default ProfileStatus
